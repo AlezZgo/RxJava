a0a9a83f6355bf076d59d1e952db83387420ae06
Alexander <alez.grante@gmail.com>
2022-03-24 21:43:31 +0300

    Subscribe

--- app/src/main/java/com/example/rxjava/MainActivity.kt
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -3,12 +3,39 @@ package com.example.rxjava
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rxjava.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.button.setOnClickListener {
+            RxHelper.text = Random.nextInt().toString()
+        }
+
+        RxHelper.getObservable().subscribe(object : Observer<String>{
+            override fun onSubscribe(d: Disposable) {
+
+            }
+
+            override fun onNext(t: String) {
+                binding.tv.text = t
+            }
+
+            override fun onError(e: Throwable) {
+
+            }
+
+            override fun onComplete() {
+
+            }
+
+        })
+
         setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/rxjava/RxHelper.kt
+++ app/src/main/java/com/example/rxjava/RxHelper.kt
@@ -3,8 +3,8 @@ package com.example.rxjava
 import io.reactivex.rxjava3.core.Observable
 
 
-class RxHelper {
-    private val text = "hello Rx java"
+object RxHelper {
+    var text = "hello Rx java"
     fun getObservable() : Observable<String> {
         return Observable.just(text)
     }
